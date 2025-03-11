763e4a0ad87f3a7de3f675960c983114757d32b8
mmd-yavarii <mdyavarii@gmail.com>
2025-03-11 12:32:28 +0330

    debug log in

--- src/contexts/LoginInfoProvider.jsx
+++ src/contexts/LoginInfoProvider.jsx
@@ -1,29 +1,17 @@
-import { createContext, useEffect, useState } from 'react';
+import { createContext, useEffect, useState } from "react";
 
 const LoginContext = createContext();
 
-function getCookie(name) {
-    const cookies = document.cookie.split('; ');
-    const cookie = cookies.find((row) => row.startsWith(`${name}=`));
-    return cookie ? JSON.parse(decodeURIComponent(cookie.split('=')[1])) : {};
-}
-
 function LoginInfo({ children }) {
-    const [loginData, setLoginData] = useState(getCookie);
+    const [loginData, setLoginData] = useState(JSON.parse(localStorage.getItem("loginData")) || []);
     const token = loginData.token;
     const role = loginData.role;
 
     useEffect(() => {
-        document.cookie = `loginData=${encodeURIComponent(
-            JSON.stringify(loginData),
-        )}; path=/; max-age=86400000`;
+        localStorage.setItem("loginData", JSON.stringify(loginData));
     }, [loginData]);
 
-    return (
-        <LoginContext.Provider value={{ token, role, setLoginData }}>
-            {children}
-        </LoginContext.Provider>
-    );
+    return <LoginContext.Provider value={{ token, role, setLoginData }}>{children}</LoginContext.Provider>;
 }
 
 export { LoginContext, LoginInfo as default };
